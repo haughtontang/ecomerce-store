6a33a24a6415f215b8605ebcedf08d28cfd9f490
Don Haughton <donhaughton@Dons-MacBook-Air.local>
2024-06-16 19:02:21 +0100

    added more results

--- store_src/viz/query_and_plot.py
+++ store_src/viz/query_and_plot.py
@@ -11,6 +11,7 @@ from store_src.sql.upload_data_to_db import return_conn_and_cursor_obj_for_table
     Total Sales by Artist:
         - "Can you provide the total sales for each artist?"
         - "Who are the top 5 artists based on billboard rating?"
+            - This is kind of done since I limited the data to the top 10
         - "What is the average album sales across all artists?"
         - "Which artists have more than 1 million streaming followers?"
         - "Can you compare the total sales and album sales for each artist?"
@@ -37,10 +38,26 @@ def plot_total_sales_per_artist(database_path: Path, table_name: str, output_pat
     ax.set_xlabel('Artist')
     ax.set_ylabel('Total Sales')
     ax.set_title('Total Sales by Artist')
+    ax.spines[['right', 'top']].set_visible(False)
     plt.tight_layout()
     plt.savefig(output_path)
     plt.close()
     conn.close()
+
+
+def print_avg_album_sales_for_all_artists(database_path: Path, table_name: str):
+    conn, cur = return_conn_and_cursor_obj_for_table(database_path=database_path)
+    cur.execute(f"SELECT album_sales FROM {table_name};")
+    query_data = cur.fetchall()
+    print(f"Avg Album sales across all artists: {np.mean(query_data)}")
+
+
+def print_artists_with_more_than_one_million_followers(database_path: Path, table_name: str):
+    conn, cur = return_conn_and_cursor_obj_for_table(database_path=database_path)
+    cur.execute(f"SELECT artist FROM {table_name} WHERE streaming_followers > 1000000;")
+    query_data = cur.fetchall()
+    print(f"Number of artists with more than one million followers: {len(query_data)}\n"
+          f"Artists with > one million followers: {[i[0] for i in query_data]}")
 
 
 if __name__ == '__main__':
@@ -48,3 +65,5 @@ if __name__ == '__main__':
     db_path = Path(f"/Users/donhaughton/Documents/PycharmProjects/ecommerce-store/data/{table_name}.db")
     output_dir = Path(f"/Users/donhaughton/Documents/PycharmProjects/ecommerce-store/data/query_plots")
     plot_total_sales_per_artist(database_path=db_path, table_name=table_name, output_path=output_dir.joinpath("top_10_sales.png"))
+    print_avg_album_sales_for_all_artists(database_path=db_path, table_name=table_name)
+    print_artists_with_more_than_one_million_followers(database_path=db_path, table_name=table_name)
